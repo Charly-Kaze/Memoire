9f2e074ee238f22a56cf15c17a76721a8183c4e3
Charly-Kaze <charlykouemeko@gmail.com>
2025-04-30 20:08:32 +0200

    Update setup_and_run.bat
    all the requirements to launch the application written

--- setup_and_run.bat
+++ setup_and_run.bat
@@ -1,1 +1,13 @@
+@echo off
+echo Création de l'environnement virtuel...
+python -m venv venv
+call venv\Scripts\activate
+
+echo Installation des dépendances...
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo Lancement de l'application Streamlit...
+streamlit run UI.py
+pause
 
